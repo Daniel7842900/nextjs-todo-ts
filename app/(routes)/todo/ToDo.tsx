6087a733082ce4fe9273985024054bce6ccdc274
daniel7842900 <kyungmin.dan.lim@gmail.com>
2024-01-05 14:59:10 -0800

    add basic structure of the project

---
+++ app/(routes)/todo/ToDo.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+import Card from "@/app/_components/Card";
+
+export default function ToDo(props) {
+  return (
+    <div className="flex flex-col min-w-[1446px] border border-sky-200 items-center">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
---
+++ app/_components/Card.tsx
@@ -0,0 +1,5 @@
+export default function Card() {
+  return (
+    <div className="w-8/12 h-24 border border-red-200 bg-white rounded-lg my-2"></div>
+  );
+}
