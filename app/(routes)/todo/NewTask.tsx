5313f087960eb43269d5cfaeac0ffdbcf0d03764
daniel7842900 <kyungmin.dan.lim@gmail.com>
2024-01-07 16:28:58 -0800

    add onclick event on new task button to render new card

--- app/(routes)/todo/NewTask.tsx
+++ app/(routes)/todo/NewTask.tsx
@@ -1,11 +1,18 @@
 import AddButton from "@/app/(routes)/todo/AddButton";
 import React from "react";
 
-export default function NewTask() {
+interface Props {
+  handleOnClick: (e: React.MouseEvent<HTMLDivElement>) => void;
+}
+
+export default function NewTask({ handleOnClick }: Props) {
   return (
-    <div className="w-8/12 min-h-[96px] flex rounded-lg my-2 justify-center items-center">
-      <AddButton styles="mx-2" />
-      <span className="text-gray-500 text-xl">Add Task</span>
+    <div
+      className="w-8/12 min-h-[96px] flex rounded-lg my-2 justify-center items-center"
+      onClick={handleOnClick}
+    >
+      <AddButton styles="mx-2 border-0 " />
+      <span className="text-gray-500 text-2xl">Add Task</span>
     </div>
   );
 }
--- app/(routes)/todo/ToDo.tsx
+++ app/(routes)/todo/ToDo.tsx
@@ -5,24 +5,32 @@ import NewTask from "@/app/(routes)/todo/NewTask";
 
 export default function ToDo(props) {
   const [tasks, setTasks] = useState([]);
+  const [isNewTaskClicked, setNewTaskClicked] = useState(false);
+  const handleNewTaskOnClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    setNewTaskClicked(!isNewTaskClicked);
+  };
 
   return (
     <div className="flex flex-col min-w-[1446px] border border-sky-200 items-center">
       <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <NewTask />
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {/*<Card />*/}
+      {!isNewTaskClicked ? (
+        <NewTask handleOnClick={handleNewTaskOnClick} />
+      ) : (
+        <Card />
+      )}
     </div>
   );
 }
