210b5ed33a3fb0d69b74b4d616d86a90e178aab6
daniel7842900 <kyungmin.dan.lim@gmail.com>
2024-01-07 16:14:54 -0800

    add onchange function for textarea

--- app/(routes)/todo/ToDo.tsx
+++ app/(routes)/todo/ToDo.tsx
@@ -1,10 +1,11 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Card from "@/app/_components/Card";
-import AddButton from "@/app/(routes)/todo/AddButton";
 import NewTask from "@/app/(routes)/todo/NewTask";
 
 export default function ToDo(props) {
+  const [tasks, setTasks] = useState([]);
+
   return (
     <div className="flex flex-col min-w-[1446px] border border-sky-200 items-center">
       <Card />
--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -1,7 +1,21 @@
 import Button from "@/app/_components/Button";
 import CompleteButton from "@/app/(routes)/todo/CompleteButton";
+import { useState } from "react";
+
+interface ICard {
+  content: string;
+  complete: boolean;
+}
 
 export default function Card() {
+  const [content, setContent] = useState("");
+  const [complete, isComplete] = useState(false);
+
+  const handleContentOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newContent = e.target.value;
+    setContent(newContent);
+  };
+
   return (
     <div className="w-8/12 min-h-[96px] flex border border-red-200 bg-white rounded-lg my-2 justify-between items-center">
       <div className="flex-1 mx-4">
@@ -13,7 +27,8 @@ export default function Card() {
           placeholder="Add a task..."
           rows="3"
           className="resize-none w-full outline-0 cursor-pointer focus:cursor-text"
-        ></textarea>
+          onChange={handleContentOnChange}
+        />
       </div>
       <CompleteButton styles="mr-4 hover:bg-green-500 hover:border-green-500 group" />
     </div>
