8d158531a3163c7a54ff98a75abff73063e03215
daniel7842900 <kyungmin.dan.lim@gmail.com>
2024-01-14 00:06:14 -0800

    add undo button for task

---
+++ app/(routes)/todo/UndoButton.tsx
@@ -0,0 +1,14 @@
+import { FaUndo } from "react-icons/fa";
+import IconButton from "@/app/_components/IconButton";
+
+interface Props {
+  styles?: string;
+  onComplete: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+export default function UndoButton({ styles, onComplete }: Props) {
+  return (
+    <IconButton styles={styles} onClick={onComplete}>
+      <FaUndo className="hidden w-3/4 h-3/4 group-hover:block group-hover:text-white " />
+    </IconButton>
+  );
+}
--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -1,6 +1,7 @@
 import CompleteButton from "@/app/(routes)/todo/CompleteButton";
 import React from "react";
 import CreateButton from "@/app/(routes)/todo/CreateButton";
+import UndoButton from "@/app/(routes)/todo/UndoButton";
 
 export interface ITask {
   idx: number;
@@ -42,8 +43,14 @@ export default function Card({
   };
 
   return (
-    <div className="w-8/12 min-h-[96px] flex border border-red-200 bg-white rounded-lg my-2 justify-between items-center">
-      <div className="flex-1 mx-4">
+    <div
+      className={
+        complete
+          ? "w-8/12 min-h-[96px] bg-green-500 flex border border-red-200 rounded-lg my-2 justify-between items-center "
+          : "w-8/12 min-h-[96px] bg-white flex border border-red-200 rounded-lg my-2 justify-between items-center "
+      }
+    >
+      <div className={"flex-1 mx-4"}>
         <label htmlFor="task" className="hidden">
           Add a task
         </label>
@@ -51,7 +58,12 @@ export default function Card({
           name="task"
           placeholder="Add a task..."
           rows="3"
-          className="resize-none w-full outline-0 cursor-pointer focus:cursor-text"
+          className={
+            complete
+              ? "resize-none w-full outline-0 bg-green-500 text-gray-500"
+              : "resize-none w-full outline-0 cursor-pointer focus:cursor-text bg-white"
+          }
+          readOnly={complete ? true : false}
           value={content}
           onChange={handleOnChange}
         />
@@ -60,6 +72,11 @@ export default function Card({
         <CreateButton
           styles="mr-4 hover:bg-blue-500 hover:border-blue-500 group"
           onCreate={handleCreateTaskOnClick}
+        />
+      ) : complete ? (
+        <UndoButton
+          styles="mr-4 hover:bg-yellow-500 hover:border-yellow-500 group"
+          onComplete={handleCompleteOnClick}
         />
       ) : (
         <CompleteButton
